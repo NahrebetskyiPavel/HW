cfb023eec8967ef447e5ce72e9ca03e61067f11d
Hryhorii Opanasenko <hryhorii.opanasenko@gmail.com>
2024-04-30 16:07:11 +0300

    hw

---
+++ js/L1/main.js
@@ -0,0 +1,41 @@
+let w1 = 'hello'
+let w2 = 'owu'
+let w3 = 'com'
+let w4 = 'ua'
+let w5 = 1
+let w6 = 10
+let w7 = -999
+let w8 = 123
+let w9 = 3.14
+let w10 = 2.7
+let w11 = 16
+let w12 = 2.7
+let w13 = true
+let w14 = false
+
+let firstName = 'Paul '
+let middleName = 'Mikhauylovich '
+let lastName = 'Nagrebetskyi'
+let a = 100;
+let b = '100';
+let c = true;
+
+
+console.log(w1)
+console.log(w2)
+console.log(w3)
+console.log(w4)
+console.log(w5)
+console.log(w6)
+console.log(w7)
+console.log(w8)
+console.log(w9)
+console.log(w10)
+console.log(w11)
+console.log(w12)
+console.log(w13)
+console.log(w14)
+console.log(firstName + middleName + lastName)
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
